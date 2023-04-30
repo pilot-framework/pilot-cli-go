b197a82b5f63c39e1ff97fc22233c14603dc57ab
Graham Robertson <grammar@hey.com>
2023-04-30 16:22:14 -0700

    chore: initial cli scaffolding

---
+++ cmd/server/connect.go
@@ -0,0 +1,43 @@
+/*
+Copyright © 2023 Pilot Framework
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+*/
+package server
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+// connectCmd represents the connect command
+var connectCmd = &cobra.Command{
+	Use:   "connect",
+	Short: "Used to set the connection context to a Pilot Server.",
+	Long: `To allow team members access to the Pilot Server, this command uses a token received via 'pilot server invite'
+to use for authenticating with the server.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("connect called")
+	},
+}
+
+func init() {
+	ServerCmd.AddCommand(connectCmd)
+}
